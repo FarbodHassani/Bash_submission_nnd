95b77417208ea6e67f6788ccd764569dcd1b4ad5
FarbodHassani <farbod.hassani@gmail.com>
2021-02-16 11:27:38 +0100

    Old files deleted

--- Run_all.sh
+++ Run_all.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-data1=("BigMDPL_Rockstar_z[0]_L[2500]" "BigMDPL_Rockstar_z[1]_L[2500]" "BigMDPL_Rockstar_z[05]_L[2500]" "MDPL_FOF_z[0]_L[1000]" "MDPL_FOF_z[1]_L[1000]" "MDPL_FOF_z[05]_L[1000]" "MDPL2_FOF_z[0]_L[1000]" "MDPL2_FOF_z[1]_L[1000]" "MDPL2_FOF_z[05]_L[1000]" "MDPL2_Rockstar_z[0]_L[1000]" "MDPL2_Rockstar_z[1]_L[1000]" "MDPL2_Rockstar_z[05]_L[1000]" "SMDPL_FOF_z[0]_L[400]" "SMDPL_FOF_z[1]_L[400]" "SMDPL_FOF_z[05]_L[400]" "SMDPL_Rockstar_z[0]_L[400]" "SMDPL_Rockstar_z[1]_L[400]" "SMDPL_Rockstar_z[05]_L[400]")
+data1=("SMDPL_Rockstar_z[05]_L[400]" "SMDPL_Rockstar_z[1]_L[400]" "SMDPL_Rockstar_z[0]_L[400]" "SMDPL_FOF_z[05]_L[400]" "SMDPL_FOF_z[1]_L[400]" "SMDPL_FOF_z[0]_L[400]" "MDPL2_Rockstar_z[05]_L[1000]" "MDPL2_Rockstar_z[1]_L[1000]" "MDPL2_Rockstar_z[0]_L[1000]" "MDPL2_FOF_z[05]_L[1000]" "MDPL2_FOF_z[1]_L[1000]" "MDPL2_FOF_z[0]_L[1000]" "MDPL_FOF_z[05]_L[1000]" "MDPL_FOF_z[1]_L[1000]" "MDPL_FOF_z[0]_L[1000]" "BigMDPL_Rockstar_z[05]_L[2500]" "BigMDPL_Rockstar_z[1]_L[2500]" "BigMDPL_Rockstar_z[0]_L[2500]")
+
+
 m_min1=("10" "10.5" "11" "11.5" "12" "12.5" "13" "14" "15")
 
 ll=0;
@@ -22,6 +24,6 @@ sed -e 's/m_min=10/m_min='$m_min'/g' -e 's/data_name1 = "z0m5e11.csv"/data_name1
 
 sed -e 's/python nnd_final.py/python .\/'$output_dir'\/Code_'$output_dir.py'/g' -e 's/#SBATCH -J job1/#SBATCH -J job1'$j$k$l'/g' Run_baobab.sh > Run_$output_dir.sh
 #
-#sbatch Run_number$output_dir.sh
+sbatch Run_$output_dir.sh
 done
 done
--- Run_baobab.sh
+++ Run_baobab.sh
@@ -4,15 +4,18 @@
 #SBATCH --get-user-env                                                                                                   
 #SBATCH --ntasks=1                                                                                                     
 #SBATCH --cpus-per-task=1                                                                                                
-#SBATCH -p dpt-EL7                                                                                                       
+#SBATCH -p dpt-bigmem-EL7                                                                                                      
 #SBATCH --output=slurm-test-%J.out                                                                                       
 #SBATCH -t 7-00:00:00                                                                                                     
-#SBATCH --mail-type=FAIL                                                                                                 
+#SBATCH --mail-type=FAIL
+#SBATCH --mem-per-cpu=64000                                                                                                
 
-##cd ##ROOTDIR##                                                                                                         
+##cd ##ROOTDIR##                                                                                           
 
 #source /etc/profile.modules                                                                                             
 #module load foss/2018b HDF5 GSL
+ml GCC/8.2.0-2.31.1 OpenMPI/3.1.3 Python/3.7.2 SciPy-bundle/2019.03
+
 echo Running on host `hostname`
 echo Time is `date`
 echo Directory is `pwd`
